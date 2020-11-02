315559fbbe90b572c51b4cdb743f628144cbd184
황상한 <nmc2711@inforex.co.kr>
2020-11-03 08:47:17 +0900

    리덕스 이니셜 구조 설정 및 로그인 모달 기능 구조 세팅

---
+++ client/src/components/auth/LoginModal.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import {} from "reactstrap";
+
+const LoginModal = () => {};
+
+export default LoginModal;
---
+++ client/src/redux/sagas/authSaga.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { takeEvery, call, put, all, fork } from "redux-saga/effects";
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from "../types";
+
+// Login
+
+const loginUserAPI = (loginData) => {
+  console.log(loginData, "loginData");
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+  return axios.post("api/auth", loginData, config);
+};
+
+function* loginUser(action) {
+  try {
+    const result = yield call(loginUserAPI, action.payload);
+    console.log(result);
+    yield put({
+      type: LOGIN_SUCCESS,
+      payload: result.data,
+    });
+  } catch (e) {
+    yield put({
+      type: LOGIN_FAILURE,
+      payload: e.response,
+    });
+  }
+}
+
+function* watchLoginUser() {
+  yield takeEvery(LOGIN_REQUEST, loginUser);
+}
+
+export default function* authSaga() {
+  yield all([fork(watchLoginUser)]);
+}
