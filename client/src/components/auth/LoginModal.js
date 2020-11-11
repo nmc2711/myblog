ce840a74c0d6ff7447050b8251ee25045dc44b9b
황상한 <nmc2711@inforex.co.kr>
2020-11-12 08:47:17 +0900

    로그인 모달 구현

--- client/src/components/auth/LoginModal.js
+++ client/src/components/auth/LoginModal.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-
-import { Modal, NavLink, ModalHeader, ModalBody, Alert, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import { NavLink, Modal, ModalHeader, ModalBody, Alert, Form, FormGroup, Label, Input, Button } from "reactstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { CLEAR_ERROR_REQUEST, LOGIN_REQUEST } from "../../redux/types";
 
@@ -12,8 +11,7 @@ const LoginModal = () => {
     password: "",
   });
   const dispatch = useDispatch();
-  const errorMsg = useSelector((state) => state.auth);
-
+  const { errorMsg } = useSelector((state) => state.auth);
   useEffect(() => {
     try {
       setLocalMsg(errorMsg);
@@ -40,9 +38,10 @@ const LoginModal = () => {
     e.preventDefault();
     const { email, password } = form;
     const user = { email, password };
+    console.log(user);
     dispatch({
       type: LOGIN_REQUEST,
-      data: user,
+      payload: user,
     });
   };
   return (
@@ -52,18 +51,20 @@ const LoginModal = () => {
       </NavLink>
       <Modal isOpen={modal} toggle={handleToggle}>
         <ModalHeader toggle={handleToggle}>Login</ModalHeader>
-        <ModalBody>{localMsg ? <Alert color="danger">{localMsg}</Alert> : null}</ModalBody>
-        <Form>
-          <FormGroup>
-            <Label for="email">Email</Label>
-            <Input type="email" name="email" id="email" placeholder="Email" onChange={onChange} />
-            <Label for="password">Password</Label>
-            <Input type="password" name="password" id="password" placeholder="password" onChange={onChange} />
-            <Button color="dark" style={{ marginTop: "2rem" }} block>
-              Login
-            </Button>
-          </FormGroup>
-        </Form>
+        <ModalBody>
+          {localMsg ? <Alert color="danger">{localMsg}</Alert> : null}
+          <Form onSubmit={onSubmit}>
+            <FormGroup>
+              <Label for="email">Email</Label>
+              <Input type="email" name="email" id="email" placeholder="Email" onChange={onChange} />
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" id="password" placeholder="Password" onChange={onChange} />
+              <Button color="dark" style={{ marginTop: "2rem" }} block>
+                Login
+              </Button>
+            </FormGroup>
+          </Form>
+        </ModalBody>
       </Modal>
     </div>
   );
