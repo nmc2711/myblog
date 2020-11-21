2be2e3dfafa4d7ab5ea4cbdd966c4e574160b19d
황상한 <nmc2711@inforex.co.kr>
2020-11-21 12:51:07 +0900

    프론트단 라우트 작업

--- client/src/routes/Router.js
+++ client/src/routes/Router.js
@@ -2,12 +2,30 @@ import React, { Fragment } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import AppNavbar from "../components/AppNavbar";
+import { Container } from "reactstrap";
+import { Route, Switch, Redirect } from "react-router-dom";
+//components
+import PostWrite from "./normalRoute/PostWrite";
+import PostCardList from "./normalRoute/PostCardList";
+import PostDetail from "./normalRoute/PostDetail";
+import Search from "./normalRoute/Search";
+import CategoryResult from "./normalRoute/CategoryResult";
 
 const MyRouter = () => (
   <Fragment>
     <AppNavbar />
     <Header />
-    <h1>Hello Body</h1>
+    <Container id="main-body">
+      <Switch>
+        <Route path="/" exact component={PostCardList} />
+        <Route path="/post" exact component={PostWrite} />
+        <Route path="/post/:id" exact component={PostDetail} />
+        <Route path="/post/:id/:categoryName" exact component={CategoryResult} />
+
+        <Route path="/search/:searchTerm" exact component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Container>
     <Footer />
   </Fragment>
 );
