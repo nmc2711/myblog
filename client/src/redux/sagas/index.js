f8baa21e537a2bf6dabc8a733608cddd534a3f07
황상한 <nmc2711@inforex.co.kr>
2020-11-24 08:52:33 +0900

    all commit postfile

--- client/src/redux/sagas/index.js
+++ client/src/redux/sagas/index.js
@@ -2,11 +2,13 @@ import { all, fork } from "redux-saga/effects";
 import axios from "axios";
 
 import authSage from "./authSaga";
+import postSaga from "./postSaga";
+
 import dotenv from "dotenv";
 
 dotenv.config();
 axios.defaults.baseURL = process.env.REACT_APP_BASIC_SERVER_URL;
 
 export default function* rootSaga() {
-  yield all([fork(authSage)]);
+  yield all([fork(authSage, fork(postSaga))]);
 }
---
+++ client/src/redux/sagas/postSaga.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { all, put, takeEvery, call, fork } from "redux-saga/effects";
+import { push } from "connected-react-router";
+import { POSTS_LOADING_FAILURE, POSTS_LOADING_REQUEST, POSTS_LOADING_SUCCESS } from "../types";
+
+// ALL Posts Load
+
+const loadPostAPI = () => {
+  return axios.get("/api/post");
+};
+
+function* loadPosts() {
+  try {
+    const result = yield call(loadPostAPI);
+    console.log(result, "loadPosts");
+    yield put({
+      type: POSTS_LOADING_SUCCESS,
+      payload: result.data,
+    });
+  } catch (e) {
+    yield put({
+      type: POSTS_LOADING_FAILURE,
+      payload: e,
+    });
+    yield push("/");
+  }
+}
+function* watchLoadPosts() {
+  yield takeEvery(POSTS_LOADING_REQUEST, loadPosts);
+}
+
+export default function* postSaga() {
+  yield all([fork[watchLoadPosts]]);
+}
